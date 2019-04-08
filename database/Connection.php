512cacebdc46abaac5faff1ae23ddba2e5736b19
Daniel Lerner <d_lerner@hotmail.com>
2019-04-08 09:02:28 -0400

    Recfactored the code and created a Connection class

---
+++ database/Connection.php
@@ -0,0 +1,14 @@
+<?php
+
+class Connection
+{
+  
+  public static function make()
+  {
+    try {
+        return new PDO('mysql:host=127.0.0.1:8889; dbname=mytodo', 'root', 'root' );
+    }catch(PDOException $e) {
+      die($e->getMessage());
+    }
+  }
+}
--- functions.php
+++ functions.php
@@ -1,22 +1,11 @@
 <?php
 
-function connectToDB()
-{
-
-    try {
-        return new PDO('mysql:host=127.0.0.1:8889; dbname=mytodo', 'root', 'root' );
-    } catch (PDOException $e) {
-        die('Could not connect');
-    }
-
-}
-
 function fetchAllTasks($pdo)
 {
     $statement = $pdo->prepare ('select * from todos');
     $statement->execute();
 
-    return $statement->fetchAll(PDO::FETCH_OBJ);
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
 }
 
 
